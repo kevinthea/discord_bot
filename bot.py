aec9608d2e2d3a382ea42055abda7020b929c872
kevin <kevin.thea@gmail.com>
2021-08-20 07:37:16 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('-k'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
